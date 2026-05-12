51a996780db5802fcee06fbc33bce30ebc15c08a
rushi <rushi@dev.com>
2026-05-12 11:59:26 +0530

    pass context load integration test

--- src/test/java/com/rm/ums/TestcontainersConfiguration.java
+++ src/test/java/com/rm/ums/TestcontainersConfiguration.java
@@ -3,10 +3,12 @@ package com.rm.ums;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@Profile("ci")
 class TestcontainersConfiguration {
 
     @Bean

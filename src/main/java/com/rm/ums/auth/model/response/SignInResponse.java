92c4bfe8a1b738250f3d5efe67649dcdc240fc83
rushi <rushi@dev.com>
2026-05-22 13:43:15 +0530

    GH-2 : Implemented temporary signIn api.

---
+++ src/main/java/com/rm/ums/auth/model/response/SignInResponse.java
@@ -0,0 +1,12 @@
+package com.rm.ums.auth.model.response;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class SignInResponse {
+
+    private Map<String, Object> metaData;
+
+}
---
+++ src/main/java/com/rm/ums/auth/services/UserService.java
@@ -0,0 +1,32 @@
+package com.rm.ums.auth.services;
+
+import com.rm.ums.auth.model.response.SignInResponse;
+import com.rm.ums.common.model.response.CustomResponse;
+import com.rm.ums.common.repositories.UrlStatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.rm.ums.common.enums.SignInApiMetadataMappingNameEnum.urlStatusList;
+import static com.rm.ums.common.enums.UmsResponseMessageEnum.SIGNED_IN_SUCCESSFULLY;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UrlStatusRepository urlStatusRepo;
+
+    public CustomResponse signIn() {
+        SignInResponse signInResponse = new SignInResponse();
+        signInResponse.setMetaData(prepareMetaData());
+        return CustomResponse.success(signInResponse, SIGNED_IN_SUCCESSFULLY);
+    }
+
+    private Map<String, Object> prepareMetaData() {
+        Map<String, Object> metaData = new HashMap<>();
+        metaData.put(urlStatusList.name(), urlStatusRepo.findUrlStatusList());
+        return metaData;
+    }
+}

137de05d3ea06b21ed0d8bef5be1ff597439cf00
rushi <rushi@dev.com>
2026-05-26 12:26:09 +0530

    GH-8 : added transaction read only true in url service

--- src/main/java/com/rm/ums/url/services/FetchUrlService.java
+++ src/main/java/com/rm/ums/url/services/FetchUrlService.java
@@ -14,6 +14,7 @@ import com.rm.ums.url.model.response.FetchUrlResponseData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +23,7 @@ import static com.rm.ums.common.enums.MenuEnum.MY_SHORT_URLS;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 public class FetchUrlService {
 
     private final HeaderHelper headerHelper;

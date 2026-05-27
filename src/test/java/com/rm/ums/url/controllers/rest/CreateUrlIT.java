bfe9eb97f5b7d41a152f34127a32d06c5aa679e7
rushi <rushi@dev.com>
2026-05-27 18:38:36 +0530

    GH-12 : write the test if title null or originalUrl null

--- src/test/java/com/rm/ums/url/controllers/rest/CreateUrlIT.java
+++ src/test/java/com/rm/ums/url/controllers/rest/CreateUrlIT.java
@@ -10,6 +10,8 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class CreateUrlIT extends AbstractIT {
 
@@ -51,5 +53,53 @@ public class CreateUrlIT extends AbstractIT {
                 response.asString(),
                 true
         );
+    }
+
+    @Test
+    public void should_return_create_url_response_title_not_in_input() throws Exception {
+
+        String requestPayload = """
+                {
+                    "originalUrl": "https://example.com",
+                    "slug":"test"
+                }
+                """;
+        String expectedResponse =
+                TestFileUtils.readFile(CREATE_URL_RESPONSE_JSON);
+
+        Response response =
+                umsRequest()
+                        .header(REQUEST_HEADER_ROLE_ID_KEY, CUSTOMER_ROLE_ID)
+                        .contentType(ContentType.JSON)
+                        .body(requestPayload)
+                        .when()
+                        .post(ENDPOINT_CREATE_URL);
+
+        response.then()
+                .body("message",equalTo("title is required"));
+    }
+
+    @Test
+    public void should_return_create_url_response_original_url_not_in_input() throws Exception {
+
+        String requestPayload = """
+                {
+                    "title": "Test URL",
+                    "slug":"test"
+                }
+                """;
+        String expectedResponse =
+                TestFileUtils.readFile(CREATE_URL_RESPONSE_JSON);
+
+        Response response =
+                umsRequest()
+                        .header(REQUEST_HEADER_ROLE_ID_KEY, CUSTOMER_ROLE_ID)
+                        .contentType(ContentType.JSON)
+                        .body(requestPayload)
+                        .when()
+                        .post(ENDPOINT_CREATE_URL);
+
+        response.then()
+                .body("message",equalTo("original url is required"));
     }
 }

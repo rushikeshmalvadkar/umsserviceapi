6ed2fce1e18e0d8a52bab2c66a7352f80c47aef6
rushi <rushi@dev.com>
2026-05-28 16:39:06 +0530

    GH-8 : write the test for fetch url api for active urls and all urls

--- src/test/java/com/rm/ums/url/controllers/rest/FetchUrlIT.java
+++ src/test/java/com/rm/ums/url/controllers/rest/FetchUrlIT.java
@@ -1,8 +1,14 @@
 package com.rm.ums.url.controllers.rest;
 
+import com.rm.ums.TestFileUtils;
 import com.rm.ums.common.AbstractIT;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.test.context.jdbc.Sql;
+
+import static org.hamcrest.Matchers.equalTo;
 
 @Sql(
         scripts = "/sql/insert/insert-urls.sql",
@@ -10,10 +16,72 @@ import org.springframework.test.context.jdbc.Sql;
 )
 public class FetchUrlIT extends AbstractIT {
 
+    private static final String ENDPOINT_FETCH_URLS = "/urls/fetch-urls";
+    private static final String FETCH_ALL_URLS_RESPONSE_JSON_FILE_PATH = "responses/fetch-all-urls-response.json";
+    private static final String FETCH_ACTIVE_URLS_RESPONSE_JSON_FILE_PATH = "responses/fetch-active-urls-response.json";
+
+
     @Test
-    void should_return_fetch_urls_response() {
+    void should_return_all_fetch_urls_response() throws Exception {
+
+        String request = "{}";
+
+        String expectedResponse =
+                TestFileUtils.readFile(
+                        FETCH_ALL_URLS_RESPONSE_JSON_FILE_PATH);
 
 
+        Response response =
+                umsRequest()
+                        .header(REQUEST_HEADER_ROLE_ID_KEY, CUSTOMER_ROLE_ID)
+                        .contentType(ContentType.JSON)
+                        .body(request)
+                        .when()
+                        .post(ENDPOINT_FETCH_URLS);
+
+        response.then()
+                .statusCode(200);
+
+        JSONAssert.assertEquals(
+                expectedResponse,
+                response.asPrettyString(),
+                true
+        );
+
+    }
+
+    @Test
+    void should_return_active_fetch_urls_response() throws Exception {
+
+        String request = """
+                {
+                    "urlStatusId": 1
+                
+                }
+                """;
+
+        String expectedResponse =
+                TestFileUtils.readFile(
+                        FETCH_ACTIVE_URLS_RESPONSE_JSON_FILE_PATH);
+
+
+        Response response =
+                umsRequest()
+                        .header(REQUEST_HEADER_ROLE_ID_KEY, CUSTOMER_ROLE_ID)
+                        .contentType(ContentType.JSON)
+                        .body(request)
+                        .when()
+                        .post(ENDPOINT_FETCH_URLS);
+
+        response.then()
+                .statusCode(200)
+                .body("data.data.size()", equalTo(3));
+
+        JSONAssert.assertEquals(
+                expectedResponse,
+                response.asPrettyString(),
+                true
+        );
 
     }
 }
